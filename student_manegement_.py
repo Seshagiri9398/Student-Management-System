794a327bf52c1157757af555631c48ace3a39750
seshagiri <seshagirithananki@gmail.com>
2026-07-16 14:52:22 +0530

    add student function build

--- student_manegement_.py
+++ student_manegement_.py
@@ -1,16 +1,37 @@
 #step 1
 class Student:
-    def __init__(self,id,name,age,course):
-        self.id = id
-        self.name = name
+    def __init__(self,student_id,student_name,age,course,department):
+        self.student_id = student_id
+        self.student_name = student_name
         self.age = age
         self.course = course
+        self.department = department
 
     def display(self):
-        print(f"ID: {self.id}")
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Course: {self.course}")
+        print(f"Student ID: {self.student_id}")
+        print(f"Student Name: {self.student_name}")
+        print(f"Student Age: {self.age}")
+        print(f"Student Course: {self.course}")
+        print(f"Student Department:{self.department}")
 
-s1 = Student(1,"Seshagiri",23,"MCA")
-s1.display()
+
+class StudentManagementSystem:
+    def __init__(self):
+        self.students = []
+
+
+    def add_student(self):
+        student_id = int(input("Enter student id:"))
+        student_name = input("enter the student name:")
+        age = int(input("Enter the age:"))
+        course = input("Enter your course:")
+        department = input("Enter the department:")
+
+        student = Student(student_id,student_name,age,course,department)
+
+        self.students.append(student)
+        print("student added Successfully ")
+sms = StudentManagementSystem()
+sms.add_student()
+
+    
