9907521a3a8bd93ed444e9b88493f40709c07868
seshagiri <seshagirithananki@gmail.com>
2026-07-23 15:19:26 +0530

    added searching feature

--- student_manegement_.py
+++ student_manegement_.py
@@ -7,7 +7,7 @@ class Student:
         self.course = course
         self.department = department
 
-    def display(self):
+    def display_details(self):
         print(f"Student ID: {self.student_id}")
         print(f"Student Name: {self.student_name}")
         print(f"Student Age: {self.age}")
@@ -31,7 +31,49 @@ class StudentManagementSystem:
 
         self.students.append(student)
         print("student added Successfully ")
-sms = StudentManagementSystem()
-sms.add_student()
+
+    def search_student(self):
+        search_id = int(input("Enter the Search ID:"))
+
+
+        for student in self.students:
+            if search_id == student.student_id:
+                student.display_details()
+                return 
+        print("Student not found")
+
+
+    def view_students(self):
+        if len(self.students) == 0:
+            print("no Student Found")
+        else:
+            for student in self.students:
+                student.display_details()
 
     
+sms = StudentManagementSystem()
+
+while True:
+    print("\n ==Student Management System ==")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. search Student")
+    print("4. Exit")
+
+    choice = input("Enter your choice:")
+
+    if choice == "1":
+        sms.add_student()
+
+    elif choice == "2":
+        sms.view_students()
+
+    elif choice == "3":
+        sms.search_student()
+
+    elif choice == "4":
+        print("Thank you!")
+        break
+    else:
+        print("Invalid choice")
+
